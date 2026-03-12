23a67cc07436cdd6ef389ab876ba7820eb81a413
Bartosz Andryszczyk <s33847@pjwstk.edu.pl>
2026-03-12 11:26:54 +0100

    Initial project

---
+++ apbd-cw1-git-s33847/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int x=10;
+        int y=20;
+        int w=x+y;
+        System.out.println("Dodawanie: "+x+" + "+y+" = "+ w);
+        }
+    }
