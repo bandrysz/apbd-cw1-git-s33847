feda23fc83b539ce574f739b68d2f7a49de0181d
Bartosz Andryszczyk <s33847@pjwstk.edu.pl>
2026-03-12 11:34:52 +0100

    main: dodanie potegowania

--- apbd-cw1-git-s33847/src/Main.java
+++ apbd-cw1-git-s33847/src/Main.java
@@ -2,13 +2,15 @@ public class Main {
     public static void main(String[] args) {
 
         int x=10;
-        int y=20;
+        int y=5;
         int dod=x+y;
         System.out.println("Dodawanie: "+x+" + "+y+" = "+ dod);
         int odej=x-y;
         System.out.println("Odejmowanie: "+x+" - "+y+" = "+odej );
         float mnoz=x*y;
-        System.out.println("Odejmowanie: "+x+" * "+y+" = "+mnoz );
+        System.out.println("Odejmowanie: "+x+" * "+y+" = "+mnoz);
+        double pot=Math.pow(x,y);
+        System.out.println("Potegowanie: "+x+" ^ "+y+" = "+pot);
 
 
 
